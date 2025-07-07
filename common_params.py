8ad89036201414ecc68a59a8f204d829be92144b
VansleepYeah <3308607663@qq.com>
2025-07-07 13:14:41 +0800

    Add files via upload

---
+++ common_params.py
@@ -0,0 +1,19 @@
+project_name = 'kbhome'
+company = 'bcs'
+
+
+headers_html_raw = '''
+Host: www.kbhome.com
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate, br, zstd
+DNT: 1
+Sec-GPC: 1
+Connection: keep-alive
+Upgrade-Insecure-Requests: 1
+Sec-Fetch-Dest: document
+Sec-Fetch-Mode: navigate
+Sec-Fetch-Site: none
+Sec-Fetch-User: ?1
+'''
